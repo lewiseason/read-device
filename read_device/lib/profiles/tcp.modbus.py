a2b9b32ff909aa0287e8107a78ccab8dbe9ab201
Lewis Eason <me@lewiseason.co.uk>
2015-04-17 10:54:28 +0100

    Abstract "retry" logic into a decorator
    This will be used in many places when trying to read unreliable
    hardware - separate administration from business logic etc etc

--- read_device/lib/profiles/tcp.modbus.py
+++ read_device/lib/profiles/tcp.modbus.py
@@ -1,11 +1,9 @@
-from time import sleep
-from pymodbus.client.sync import ModbusTcpClient
-import pymodbus.exceptions as mb_exceptions
+import pymodbus.client.sync
+import pymodbus.exceptions
 
 from read_device.lib.resources import BaseProfile, Property, requires_configuration
 from read_device.lib.decoders import decode
-from read_device.lib.errors import *
-
+from read_device.lib.decorators import *
 
 class TCPModbus(BaseProfile):
 
@@ -36,38 +34,12 @@ class TCPModbus(BaseProfile):
 		self.execute()
 
 	@requires_configuration
+	@attempts(3, AttributeError, pymodbus.exceptions.ConnectionException)
 	def read(self, property):
-		attempts  = 1
-		response  = None
-		exception = None
+		if   property.mode is 3: method = self.client.read_holding_registers
+		elif property.mode is 4: method = self.client.read_input_registers
 
-		while True:
-			try:
-				if property.mode is 3:
-					response = self.client.read_holding_registers(property.address, property.words, unit=self.slave).registers
-				elif property.mode is 4:
-					response = self.client.read_input_registers(property.address, property.words, unit=self.slave).registers
-
-			except Exception as e:
-				exception = e
-				sleep(0.2)
-
-			finally:
-				attempts += 1
-				if response or attempts > 3:
-					break
-
-		if response:
-			decoder = decode[self.encoding]
-			property.populate(decoder(response))
-		# TODO: Waiting for queued exceptions.
-		# When an exception is raised, pop it on a queue and have the formatter
-		# decide what to do with it *at the end*
-		# else:
-		# 	message = "The device did not respond while trying to retrieve %s" % property.name
-		# 	if exception:
-		# 		message += " (The original error was a %s)" % exception.__class__.__name__
-
-		# 	raise ResponseError(message)
+		response = method(property.address, property.words, unit=self.slave).registers
+		property.populate(self.decoder(response))
 
 profile = TCPModbus
